604839e8d0207dea67ca9f3736ef0361bcac950d
sivaarthigunasekaran <sivaarthigunsekaran@gmail.com>
2026-02-23 16:37:10 +0530

    Add files via upload

---
+++ digitextraction.java
@@ -0,0 +1,14 @@
+// digit extaction
+class digitextraction {
+    public static void main(String []args){
+     int n=123;
+     while(n>0){
+        int dig =n%10;//23
+        System.out.println(dig);
+        n=n/10;
+     }
+
+    
+    }// REVERSE PROGRAM
+
+    }
